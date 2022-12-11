5125cedf851a1fc44bc1961e740e98765de57850
satish-kuncha <satishreddyk@gmail.com>
2022-12-11 23:32:43 +0530

    text corrected

--- src/component/RemoteHeader.tsx
+++ src/component/RemoteHeader.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Header() {
   return (
 
-          <div> <p style={{color: 'white', backgroundColor: 'blue'}}> <b> Text Coponent from NeoDash - via Webpack Module Federation!</b></p></div>
+          <div> <p style={{color: 'white', backgroundColor: 'blue'}}> <b> Text Component from NeoDash - via Webpack Module Federation!</b></p></div>
 
   );
 }
